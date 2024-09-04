04d78db6906b0d6c925f31824ca6aaf4ce11df66
vae <66442608+sylviameows@users.noreply.github.com>
2024-09-04 00:14:02 -0500

    spawn at spawnpoint

--- core/src/main/java/io/github/sylviameows/flask/listeners/JoinListener.java
+++ core/src/main/java/io/github/sylviameows/flask/listeners/JoinListener.java
@@ -1,8 +1,11 @@
 package io.github.sylviameows.flask.listeners;
 
 import io.github.sylviameows.flask.Flask;
+import io.github.sylviameows.flask.api.FlaskAPI;
 import io.github.sylviameows.flask.managers.PlayerManagerImpl;
 import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -15,5 +18,10 @@ public class JoinListener implements Listener {
     @EventHandler
     private void join(PlayerJoinEvent event) {
         PlayerManagerImpl.instance().add(event.getPlayer());
+
+        Player player = event.getPlayer();
+        player.setGameMode(GameMode.ADVENTURE);
+        player.getInventory().clear();
+        player.teleportAsync(FlaskAPI.instance().getSpawnLocation());
     }
 }
